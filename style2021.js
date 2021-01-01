3df4c682b296623b32162bce6da56b4de496de7e
ommaraxx2015 <76623269+ommaraxx2015@users.noreply.github.com>
2021-01-01 20:16:37 +0200

    Update style2021.js

--- style2021.js
+++ style2021.js
@@ -2,4 +2,88 @@ $['getScript']('https://rawcdn.githack.com/ommaraxx2015/Omar_AL-Haded-Des/6d32c2
 $('<div class=\'fr borderg\' style=\'padding:2px;background-color:white;float:left;\'> استايلات و ارشفة :<a href=\'chatnsh.com\'>عمر الحديد</a></div>')['insertAfter']($('.loginstat'));
 $('.checkbox label')['html'](`${'<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">'}`);
 $('.nav-tabs')['addClass']('fl')['css']('width', '100%');
-$(`${'<link rel="stylesheet" type="text/css" href="">\x0D\x0A\x0D\x0A<link href="https://rawcdn.githack.com/ommaraxx2015/Omar_AL-Haded-Des/6d32c2f09ce0018f1fa4c2a3bcd14384a475cf31/2021.css" rel="stylesheet" type="text/css" />\x0D\x0A\x0D\x0A<style>\x0D\x0A\x0D\x0Ainput#stealth {\x0D\x0A    margin-left: -17px;\x0D\x0A}\x0D\x0A\x0D\x0Adiv#tlogins img.fitimg.fl.u-pic {\x0D\x0A    border-radius: 5px 15px 5px 5px;\x0D\x0A}\x0D\x0A\x0D\x0A.border {\x0D\x0A    box-shadow: inset 1px 1px 1px 1px rgba(0,0,0,.075), 0 0 5px rgb(146 146 146 / 64%);\x0D\x0A    border: 1px solid white;\x0D\x0A}\x0D\x0A\x0D\x0A#ALHADED3 {\x0D\x0A    text-decoration: dotted;\x0D\x0A    color: #fff;\x0D\x0A    margin-left: 2px!important;\x0D\x0A    display: inline-block;\x0D\x0A	position: absolute;\x0D\x0A    margin-right: 4.4px!important\x0D\x0A}\x0D\x0A\x0D\x0Aa.label.label-primary.fl img.fl {display: none;}\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A.fr.borderg, .fr.borderg a {\x0D\x0A    font-size: 10px!important;\x0D\x0A    padding: 0 5px!important;\x0D\x0A    position: relative;\x0D\x0A    color: #ffffff;\x0D\x0A}\x0D\x0A.fr.borderg {\x0D\x0A    border: 0px;\x0D\x0A    margin-top: 0;\x0D\x0A    height: 0;\x0D\x0A\x0D\x0A}\x0D\x0A\x0D\x0A\x0D\x0Alabel.label.label-primary.mini.fl {\x0D\x0A    padding: 1px;\x0D\x0A    border-radius: 15px!important;\x0D\x0A	margin-top: 1px;\x0D\x0A}\x0D\x0A\x0D\x0A\x0D\x0A.hmsg {background-color: rgb(235, 241, 255)!important;}\x0D\x0A#ALHADED1 {margin-bottom: 2px;font-family: \'El Messiri\',FontAwesome!important;    background-image: linear-gradient(90deg,#d0d0d0 0,#bababa 10%,#908f8f 51%,#bababa 89%,#d0d0d0 100%);color: #f9f9f9;border-radius: 3px;padding: 4px 0!important;padding-bottom: 3px;}\x0D\x0A\x0D\x0A.ae.fa.label.label-primary.fa-user {\x0D\x0A    border-radius: 0 0 15px 0;\x0D\x0A}\x0D\x0A\x0D\x0A.ae.fa.chats.label.fa-comment.label-primary {\x0D\x0A    border-radius: 0 0 0 15px;\x0D\x0A}\x0D\x0A\x0D\x0Alabel.ae.fa.chats.label.fa-comment.label-warning {\x0D\x0A    border-radius: 0 0 0 15px;\x0D\x0A}\x0D\x0A\x0D\x0A.ae.fa.label.label-primary.fa-users {\x0D\x0A    border-radius: 0 0 15px 0;\x0D\x0A    margin-left: 10px!important;\x0D\x0A}\x0D\x0A\x0D\x0A.ae.fa.label.fa-commenting-o.label-warning {\x0D\x0A    border-radius: 0 0 0 15px;\x0D\x0A}\x0D\x0A\x0D\x0A.ae.label.label-primary.fa.fa-gear {\x0D\x0A    color: #ffd900;\x0D\x0A    border-radius: 0 0 15px 0;\x0D\x0A    margin-left: 10px!important;\x0D\x0A}\x0D\x0A\x0D\x0Alabel.ae.fa.label.fa-commenting-o.label-primary {\x0D\x0A    border-radius: 0px 0px 0px 15px;\x0D\x0A}\x0D\x0A\x0D\x0A</style>\x0D\x0A'}`)['insertBefore']('body')
+$(`<link rel="stylesheet" type="text/css" href="">
+  <link href="https://rawcdn.githack.com/ommaraxx2015/Omar_AL-Haded-Des/6d32c2f09ce0018f1fa4c2a3bcd14384a475cf31/2021.css" rel="stylesheet" type="text/css">
+  <style>
+
+input#stealth {
+    margin-left: -17px;
+}
+
+div#tlogins img.fitimg.fl.u-pic {
+    border-radius: 5px 15px 5px 5px;
+}
+
+.border {
+    box-shadow: inset 1px 1px 1px 1px rgba(0,0,0,.075), 0 0 5px rgb(146 146 146 / 64%);
+    border: 1px solid white;
+}
+
+#ALHADED3 {
+    text-decoration: dotted;
+    color: #fff;
+    margin-left: 2px!important;
+    display: inline-block;
+	position: absolute;
+    margin-right: 4.4px!important
+}
+
+a.label.label-primary.fl img.fl {display: none;}
+
+
+
+.fr.borderg, .fr.borderg a {
+    font-size: 10px!important;
+    padding: 0 5px!important;
+    position: relative;
+    color: #ffffff;
+}
+.fr.borderg {
+    border: 0px;
+    margin-top: 0;
+    height: 0;
+
+}
+
+
+label.label.label-primary.mini.fl {
+    padding: 1px;
+    border-radius: 15px!important;
+	margin-top: 1px;
+}
+
+
+.hmsg {background-color: rgb(235, 241, 255)!important;}
+#ALHADED1 {margin-bottom: 2px;font-family: 'El Messiri',FontAwesome!important;    background-image: linear-gradient(90deg,#d0d0d0 0,#bababa 10%,#908f8f 51%,#bababa 89%,#d0d0d0 100%);color: #f9f9f9;border-radius: 3px;padding: 4px 0!important;padding-bottom: 3px;}
+
+.ae.fa.label.label-primary.fa-user {
+    border-radius: 0 0 15px 0;
+}
+
+.ae.fa.chats.label.fa-comment.label-primary {
+    border-radius: 0 0 0 15px;
+}
+
+label.ae.fa.chats.label.fa-comment.label-warning {
+    border-radius: 0 0 0 15px;
+}
+
+.ae.fa.label.label-primary.fa-users {
+    border-radius: 0 0 15px 0;
+    margin-left: 10px!important;
+}
+
+.ae.fa.label.fa-commenting-o.label-warning {
+    border-radius: 0 0 0 15px;
+}
+
+.ae.label.label-primary.fa.fa-gear {
+    color: #ffd900;
+    border-radius: 0 0 15px 0;
+    margin-left: 10px!important;
+}
+
+label.ae.fa.label.fa-commenting-o.label-primary {
+    border-radius: 0px 0px 0px 15px;
+}
+</style>`).insertBefore('body');
